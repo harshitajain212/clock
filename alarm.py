bea0e4950e3f76d16b74879a1486ee0df0ea18f9
Harshita jain <105262269+harshitajain212@users.noreply.github.com>
2023-07-02 16:56:53 +0530

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,29 +1,13 @@
-import datetime
-import time
-import winsound
+from tkinter.ttk import *
+from tkinter import *
 
-def set_alarm():
-    alarm_time = input("Enter the alarm time in HH:MM format: ")
-    alarm_hour, alarm_minute = map(int, alarm_time.split(':'))
+bg_color="#ffffff"
+col="#566FCC6"
+window =Tk()
+window.title(" ")
+window.geometry("350x150")
+window.configure(bg=bgcolor)
 
-    while True:
-        now = datetime.datetime.now()
-        current_hour = now.hour
-        current_minute = now.minute
-
-        if current_hour == alarm_hour and current_minute == alarm_minute:
-            print("Time's up! Alarm activated.")
-            play_alarm_sound()
-            break
-
-        time.sleep(10)  # Check every 10 seconds
-
-def play_alarm_sound():
-    frequency = 2500  # Set frequency (range: 37-32767 Hz)
-    duration = 3000  # Set duration in milliseconds
-
-    for _ in range(3):
-        winsound.Beep(frequency, duration)
-        time.sleep(1)
-
-set_alarm()
+frame_lines= Frame(window, width=400,height=5 bg=col)
+frame_line.grid(row=0,column=0)
+window.mainloop()
