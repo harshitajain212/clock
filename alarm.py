cc57a07e03f8afa92aa4a802328f0dfdc1de33e6
Harshita jain <105262269+harshitajain212@users.noreply.github.com>
2023-07-02 16:33:18 +0530

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,8 +1,11 @@
 from tkinter.ttk import *
 from tkinter import *
 
+bg_color="#ffffff"
 window =Tk()
 window.title(" ")
 window.geometry("350x150")
+window.configure(bg=bgcolor)
+
 
 window.mainloop()
