bd850c9ef43fdf0b2ca3716a294c79253ee89929
Harshita jain <105262269+harshitajain212@users.noreply.github.com>
2023-07-02 16:39:57 +0530

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -2,11 +2,12 @@ from tkinter.ttk import *
 from tkinter import *
 
 bg_color="#ffffff"
+col="#566FCC6"
 window =Tk()
 window.title(" ")
 window.geometry("350x150")
 window.configure(bg=bgcolor)
 
-frame_lines= Frame(window, width=400,height=5)
+frame_lines= Frame(window, width=400,height=5 bg=col)
 frame_line.grid(row=0,column=0)
 window.mainloop()
